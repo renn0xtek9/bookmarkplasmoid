52ee6387800cb644598252df99bb41df7632eef7
renn0xtek9 <renn0xtek9@laposte.net>
2020-04-08 23:47:34 +0200

    format bookmarksmodule.cpp

--- libbookmarksmodule/Tests/bookmarksmodule_test.cpp
+++ libbookmarksmodule/Tests/bookmarksmodule_test.cpp
@@ -1,54 +1,48 @@
+#include <bookmarksmodule_test.h>
 #include <QtCore/QSharedPointer>
 #include <bookmarkmodel.hpp>
-#include <bookmarksmodule_test.h>
-bookmarksmodule_test::bookmarksmodule_test(): QObject()
-{
-    
+bookmarksmodule_test::bookmarksmodule_test() : QObject() {
 }
-void bookmarksmodule_test::init()
-{
-//     m_model=QSharedPointer<Bookmarkmodel>(new Bookmarkmodel());
-    m_model=QSharedPointer<Bookmarkmodel>(new Bookmarkmodel);
-    m_model->setPathForChromeBookamarks("Bookmarks");
-    m_model->setPathForKonquerorBookmarks("konqueror_bookmarks.xml");
-    m_model->setPathForOkularBookmarks("okular_bookmarks.xml");
+void bookmarksmodule_test::init() {
+  //     m_model=QSharedPointer<Bookmarkmodel>(new Bookmarkmodel());
+  m_model = QSharedPointer<Bookmarkmodel>(new Bookmarkmodel);
+  m_model->setPathForChromeBookamarks("Bookmarks");
+  m_model->setPathForKonquerorBookmarks("konqueror_bookmarks.xml");
+  m_model->setPathForOkularBookmarks("okular_bookmarks.xml");
 }
-void bookmarksmodule_test::initTestCase()
-{
-    // will be called before the first testfunction is executed.
+void bookmarksmodule_test::initTestCase() {
+  // will be called before the first testfunction is executed.
 }
-void bookmarksmodule_test::cleanup()
-{
-    // will be called after every testfunction.
+void bookmarksmodule_test::cleanup() {
+  // will be called after every testfunction.
 }
-void bookmarksmodule_test::cleanupTestCase()
-{
-    // will be called after the last testfunction was executed.
+void bookmarksmodule_test::cleanupTestCase() {
+  // will be called after the last testfunction was executed.
 }
 
-void bookmarksmodule_test::path_are_set_correctly()
-{
-    QVERIFY2(m_model->getPathForChromeBookmarks()=="Bookmarks","Path for chrome bookmark is wrong");
-    QVERIFY2(m_model->getPathForKonquerorBookmarks()=="konqueror_bookmarks.xml","Path for Konqueror bookmark is wrong");
-    QVERIFY2(m_model->getPathForOkularBookmarks()=="okular_bookmarks.xml","Path for Okular bookmark is wrong");   
+void bookmarksmodule_test::path_are_set_correctly() {
+  QVERIFY2(m_model->getPathForChromeBookmarks() == "Bookmarks", "Path for chrome bookmark is wrong");
+  QVERIFY2(m_model->getPathForKonquerorBookmarks() == "konqueror_bookmarks.xml",
+           "Path for Konqueror bookmark is wrong");
+  QVERIFY2(m_model->getPathForOkularBookmarks() == "okular_bookmarks.xml", "Path for Okular bookmark is wrong");
 }
 
-void bookmarksmodule_test::get_correct_number_of_element_for_konqueror_bookmarks()
-{
-    int expected_number_of_row=4;
-    m_model=QSharedPointer<Bookmarkmodel>(new Bookmarkmodel);
-    m_model->setPathForKonquerorBookmarks("konqueror_bookmarks.xml");
-    m_model->ReadAllSources(true);
-    QVERIFY2(m_model->rowCount()==expected_number_of_row,"Do not get the correct number of element when reading form konqueror bookmarks");
+void bookmarksmodule_test::get_correct_number_of_element_for_konqueror_bookmarks() {
+  int expected_number_of_row = 4;
+  m_model = QSharedPointer<Bookmarkmodel>(new Bookmarkmodel);
+  m_model->setPathForKonquerorBookmarks("konqueror_bookmarks.xml");
+  m_model->ReadAllSources(true);
+  QVERIFY2(m_model->rowCount() == expected_number_of_row,
+           "Do not get the correct number of element when reading form konqueror bookmarks");
 }
 
-void bookmarksmodule_test::get_correct_number_of_element_for_okular_bookmarks()
-{
-    int expected_number_of_row=2;
-    m_model=QSharedPointer<Bookmarkmodel>(new Bookmarkmodel);
-    m_model->setPathForOkularBookmarks("okular_bookmarks.xml");
-    m_model->ReadAllSources(true);
-    QVERIFY2(m_model->rowCount()==expected_number_of_row,"Do not get the correct number of element when reading form konqueror bookmarks");
+void bookmarksmodule_test::get_correct_number_of_element_for_okular_bookmarks() {
+  int expected_number_of_row = 2;
+  m_model = QSharedPointer<Bookmarkmodel>(new Bookmarkmodel);
+  m_model->setPathForOkularBookmarks("okular_bookmarks.xml");
+  m_model->ReadAllSources(true);
+  QVERIFY2(m_model->rowCount() == expected_number_of_row,
+           "Do not get the correct number of element when reading form konqueror bookmarks");
 }
 
 QTEST_MAIN(bookmarksmodule_test)
