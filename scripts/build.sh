a8c28c9218adafe0e356244710fc7efc14e69f4b
Maxime Haselbauer <renn0xtek9@laposte.net>
2021-07-14 22:26:04 +0200

    Formatting cmake files

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -euxo pipefail 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+# Never build without formatting before
+$SCRIPT_DIR/format.sh
 cd $SCRIPT_DIR/../
 mkdir -p build 
 cd build
--- scripts/format.sh
+++ scripts/format.sh
@@ -3,10 +3,15 @@ set -euxo pipefail
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $SCRIPT_DIR/..
 QMLFORMAT=/usr/lib/qt5/bin/qmlformat
-
+CMAKEFORMAT=cmake-format
 
 IFS='' #If you want leading spaces to be kept. Remove it if not
 while read line 
 do 
     $QMLFORMAT -i "$line"
 done < <(find -name "*.qml") 
+
+while read line 
+do 
+    $CMAKEFORMAT -i "$line"
+done < <(find -name "CMakeLists.txt") 
