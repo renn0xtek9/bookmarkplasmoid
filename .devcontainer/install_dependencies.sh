364d4dc523b1eef4bd73d327db3e1313388cc0df
Maxime Haselbauer <maxime.haselbauer@luminartech.com>
2024-05-03 09:50:15 +0200

    Add a basic CI using github CI

---
+++ .devcontainer/install_dependencies.sh
@@ -0,0 +1,5 @@
+#!/bin/bash 
+set -euxo pipefail
+DEBIBIAN_FRONTEND=noninteractive
+sudo apt-get install extra-cmake-modules plasma-desktop-dev qml-module-qttest qtdeclarative5-dev libkf5iconthemes-dev build-essentials g++ python3 -y
+python3 -m pip install cmake-format
