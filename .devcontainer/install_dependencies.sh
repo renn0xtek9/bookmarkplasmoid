810d97a3ec90f6e08fe5b4b790ee2ef7ecf15cdf
Maxime Haselbauer <maxime.haselbauer@luminartech.com>
2024-05-03 11:48:40 +0200

    add qt5-default as developer dependency

--- .devcontainer/install_dependencies.sh
+++ .devcontainer/install_dependencies.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
 set -euxo pipefail
 DEBIBIAN_FRONTEND=noninteractive
-sudo apt-get install extra-cmake-modules plasma-desktop-dev qml-module-qttest qtdeclarative5-dev libkf5plasma-dev libkf5iconthemes-dev build-essential g++ python3 -y
+sudo apt-get install qt5-default plasma-desktop-dev qml-module-qttest qtdeclarative5-dev libkf5plasma-dev libkf5iconthemes-dev build-essential extra-cmake-modules g++ python3 -y
 python3 -m pip install cmake-format
