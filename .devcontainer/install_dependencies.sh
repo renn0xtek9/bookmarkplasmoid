5997a32b2f10fc496f2014bf9aa2ae02058e39ef
Maxime Haselbauer <maxime.haselbauer@luminartech.com>
2024-05-03 12:30:42 +0200

    Add missing gettext

--- .devcontainer/install_dependencies.sh
+++ .devcontainer/install_dependencies.sh
@@ -2,5 +2,5 @@
 set -euxo pipefail
 DEBIBIAN_FRONTEND=noninteractive
 sudo apt-get update
-sudo apt-get install qt5-default plasma-desktop-dev qml-module-qttest qtdeclarative5-dev libkf5plasma-dev libkf5iconthemes-dev build-essential cmake extra-cmake-modules g++ python3-pip -y
+sudo apt-get install qt5-default plasma-desktop-dev qml-module-qttest qtdeclarative5-dev libkf5plasma-dev libkf5iconthemes-dev build-essential cmake extra-cmake-modules g++ python3-pip gettext -y
 python3 -m pip install cmake-format
