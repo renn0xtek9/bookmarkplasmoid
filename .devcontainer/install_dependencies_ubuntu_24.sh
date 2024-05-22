83f7725b213ee4a6c23d6fa6ec737a637eb55b70
Maxime Haselbauer <renn0xtek9@laposte.net>
2024-05-22 11:28:50 +0000

    Fix install pipeline for Ubuntu 24.04 Using apt package for cmake-format and pre-commit

--- .devcontainer/install_dependencies_ubuntu_24.sh
+++ .devcontainer/install_dependencies_ubuntu_24.sh
@@ -3,5 +3,4 @@ set -euxo pipefail
 # shellcheck disable=SC2034
 DEBIAN_FRONTEND=noninteractive
 sudo apt-get update
-sudo apt-get install shellcheck clang-format qtbase5-dev plasma-desktop-dev qml-module-qttest qtdeclarative5-dev libkf5plasma-dev libkf5iconthemes-dev build-essential cmake extra-cmake-modules gettext g++ python3-pip gettext gdb -y
-python3 -m pip install cmake-format pre-commit
+sudo apt-get install shellcheck clang-format qtbase5-dev plasma-desktop-dev qml-module-qttest qtdeclarative5-dev libkf5plasma-dev libkf5iconthemes-dev build-essential cmake extra-cmake-modules gettext g++ python3-pip gettext gdb cmake-format pre-commit -y
