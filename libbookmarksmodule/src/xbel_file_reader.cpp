2a488960e45f23029b44b5dfb5150f5ce8d2bdab
Maxime Haselbauer <renn0xtek9@laposte.net>
2024-06-22 18:46:51 +0200

    fix setFilePath

--- libbookmarksmodule/src/xbel_file_reader.cpp
+++ libbookmarksmodule/src/xbel_file_reader.cpp
@@ -1,5 +1,6 @@
 #include <libbookmarksmodule/xbel_file_reader.h>
 
+#include <QDebug>
 #include <QLoggingCategory>
 #include <QtCore/QFile>
 #include <QtCore/QFileInfo>
@@ -11,31 +12,23 @@ XbelFileReader::XbelFileReader(XmlParserInterface& xml_parser) : XmlBookmarkRead
 
 bool XbelFileReader::setFilePath(const QString& file_path) {
   QFileInfo fileInfo(file_path);
-  if (fileInfo.exists()) {
-    return true;
-  } else {
+  if (!fileInfo.exists()) {
     qCWarning(logger) << "File does not exist: " << file_path;
     return false;
   }
-  if (fileInfo.isReadable()) {
-    return true;
-  } else {
+  if (!fileInfo.isReadable()) {
     qCWarning(logger) << "Not a file: " << file_path;
     return false;
   }
-  if (fileInfo.isReadable()) {
-    return true;
-  } else {
-    qCWarning(logger) << "File not readable: " << file_path;
-    return false;
-  }
   m_file_path = file_path;
-  return false;
+  return true;
 }
 
 void XbelFileReader::read(QStandardItem* root_item) {
   QFile xml_file(m_file_path);
   xml_file.open(QIODevice::ReadOnly);
+  qDebug() << m_file_path;
+  qDebug() << xml_file;
   QXmlStreamReader xml_stream;
   xml_stream.setDevice(&xml_file);
   m_xml_parser.read(xml_stream, root_item);
